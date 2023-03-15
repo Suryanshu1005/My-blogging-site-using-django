e69ac8fd2f55deef95557cb680d48f9ce37f4517
Suryanshu <tshuryanshu@gmail.com>
2023-03-16 01:46:17 +0530

    templates modified

--- penPundit/blog/models.py
+++ penPundit/blog/models.py
@@ -27,6 +27,9 @@ class Post(models.Model):
     cat= models.ForeignKey(Category, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='post/')
 
+    def __str__(self):
+        return self.title
+
     def image_tag(self):
         return format_html('<img src = "/media/{}" style="width:40px;height=40px;border-radius:50%" />'.format(self.image))
 
--- penPundit/blog/views.py
+++ penPundit/blog/views.py
@@ -1,14 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Category, Post
+from blog.models import Category, Post
 
 # Create your views here.
 
 # we will create objects of those classes inside models to get the data present inside those classes
 
 category = Category.objects.all()
-posts = Post.objects.all()
+
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    posts = Post.objects.all()[:11]
+    # print(posts)
+    data = {
+        'posts' : posts
+    }
+    return render(request, 'home.html', data)
